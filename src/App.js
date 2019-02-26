fd6e9035c619675c85d40ae14ff54faf009d4850
Jason Te <jason.t@ideahubsolutionsinc.com>
2019-02-26 12:47:08 +0800

    changed app text on feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h1>Hello World!</h1>
+                    <h1>I was changed on the feature branch!</h1>
                 </header>
             </div>
         );
